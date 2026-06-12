464741f5105e3c62470ff09ef7aba1f509b6d6f8
Your Name <you@example.com>
2026-06-12 14:05:42 +0700

    Cap nhat link API sang Render lan 6

--- backend/src/routes/payment.routes.js
+++ backend/src/routes/payment.routes.js
@@ -3,6 +3,10 @@ const paymentController = require("../controllers/payment.controller");
 
 const router = express.Router();
 
+// Route nhận kết quả trả về giao diện
 router.get("/vnpay-return", paymentController.vnpayReturn);
+
+// Route IPN để VNPAY cập nhật trạng thái đơn hàng ngầm
 router.get("/vnpay-ipn", paymentController.vnpayIpn);
+
 module.exports = router;
