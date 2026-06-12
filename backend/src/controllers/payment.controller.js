07c47eb1243638b42f60d72f43c8a814289f3991
Your Name <you@example.com>
2026-06-12 14:08:16 +0700

    Cap nhat link API sang Render lan 7

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -1,7 +1,6 @@
 const asyncHandler = require("../utils/asyncHandler");
 const paymentService = require("../services/payment.service");
 
-// 1. Hàm xử lý Return URL (Dành cho giao diện người dùng)
 const vnpayReturn = asyncHandler(async (req, res) => {
   const data = await paymentService.handleVnpayCallback(req.query);
 
@@ -12,22 +11,6 @@ const vnpayReturn = asyncHandler(async (req, res) => {
   });
 });
 
-// 2. Hàm xử lý IPN URL (Dành cho hệ thống VNPAY gọi ngầm)
-const vnpayIpn = asyncHandler(async (req, res) => {
-  try {
-    // Gọi hàm xử lý và cập nhật DB giống hệt Return
-    await paymentService.handleVnpayCallback(req.query);
-    
-    // VNPAY bắt buộc IPN phải trả về format này nếu thành công
-    return res.status(200).json({ RspCode: "00", Message: "Confirm Success" });
-  } catch (error) {
-    // Nếu có lỗi (ví dụ: sai chữ ký), báo lại cho VNPAY
-    return res.status(200).json({ RspCode: "99", Message: "Unknown error" });
-  }
-});
-
-// BẮT BUỘC PHẢI EXPORT CẢ 2 HÀM
 module.exports = {
   vnpayReturn,
-  vnpayIpn, 
 };
